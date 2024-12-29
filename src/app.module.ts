8a47207464a6015325ebad846872cee90df113ba
Salman <miansalmanhassan@yahoo.com>
2024-12-30 02:20:29 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,16 @@ import { TaskModule } from '@task/task.module';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      url: 'postgresql://greekatodos_user:kiMoK2WLSyi57gPpjuRioHw3T0P3lnCP@dpg-ctorkgd2ng1s73bledog-a/greekatodos',
+      type: 'postgres',
+      host: 'dpg-ctorkgd2ng1s73bledog-a',  
+      port: 5432,                          
+      username: 'greekatodos_user',        
+      password: 'kiMoK2WLSyi57gPpjuRioHw3T0P3lnCP',
+      database: 'greekatodos',
       autoLoadEntities: true,
-      synchronize: false,
-      ssl: {
-        rejectUnauthorized: false,
+      synchronize: false, // Set to false in production
+      ssl: { 
+        rejectUnauthorized: false,  // Allow SSL connections
       },
     }),
     TaskModule,
